4470007f5847e662dede5632635427699414dcb8
vivianmahr <vivianmahr@gmail.com>
2015-05-18 14:49:28 -0700

    Gitignore, Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Networked Snake Game for ICS 168
+Most of frontend taken from Flowers for the Past and modified for Snake
+(https://github.com/vivianmahr/Flowers-for-the-Past)
--- server/db/database_handler.py
+++ server/db/database_handler.py
@@ -3,8 +3,11 @@ import sqlite3
 class Database_Connection():
     def __init__(self, db_name):
         self.db = sqlite3.connect(db_name)
-
+        
     def user_in_db(self, username):
+        cursor = self.db.cursor()
+        cursor.execute('SELECT * FROM user WHERE name="?"', username)
+        print(cursor)
         return False
 
     def authenticate_user(self, username, password):
@@ -16,8 +19,22 @@ class Database_Connection():
     def print_database(self):
         print("Please implement me")
 
-
 # For Testing
 if (__name__ == "__main__"):
-    c = Database_Connection("users_test.db")
-    
+    db = Database_Connection("users_test.db")
+    print(db.user_in_db("test"))
+"""
+user(name TEXT, password TEXT, win INT, game INT, PRIMARY KEY(name))
+"""
+
+"""
+t = ('IBM',)
+c.execute('select * from stocks where symbol=?', t)
+
+# Larger example
+for t in [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
+          ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
+          ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+         ]:
+    c.execute('insert into stocks values (?,?,?,?,?)', t)
+"""
